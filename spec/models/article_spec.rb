336e81dd1157c0ab6b8c66f952e8542af42c3d07
Simon <simonlucas1996@gmail.com>
2026-02-08 13:50:18 -0300

    Add Article model specs and fix lint

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:work_order_items) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+  end
+
+  describe 'factories' do
+    it 'builds a valid article' do
+      expect(build(:article)).to be_valid
+    end
+  end
 end
